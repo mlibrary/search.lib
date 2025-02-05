7554bd06af4913f8c208a6ab2201dd6a13b0741e
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-02-05 13:31:19 -0500

    Adding main redirect and views.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require "sinatra"
 require "puma"
 
 get "/" do
-  "Hello World"
+  redirect to("/everything")
+end
+
+get "/everything" do
+  erb "Hello World"
+end
+
+not_found do
+  status 404
+  erb :"errors/404"
 end
