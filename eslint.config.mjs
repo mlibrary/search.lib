0e5670a180b6fa04c6330da302fc394cf3d12f44
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-02-05 20:53:16 +0000

    Adding linting rules and scripts.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,34 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  pluginJs.configs.all,
+  stylistic.configs['recommended-flat'],
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@stylistic/operator-linebreak': ['error', 'before'],
+      '@stylistic/quote-props': ['error', 'as-needed'],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/space-before-function-paren': ['error', 'always'],
+      '@stylistic/spaced-comment': ['error', 'always', { block: { balanced: true } }],
+      'arrow-body-style': ['error', 'always'],
+      'no-ternary': 'off',
+      'one-var': ['error', { initialized: 'never' }],
+      'sort-imports': ['error', { ignoreCase: true }]
+    }
+  }
 ];
