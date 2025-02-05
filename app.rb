bd2a4ab359d3ad9303641a7df062f62e720dd51e
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-02-05 20:45:51 +0000

    Adding static page routes, files, and styles.

--- app.rb
+++ app.rb
@@ -9,6 +9,17 @@ get "/everything" do
   erb "Hello World"
 end
 
+# Read all files in views/pages to automatically create routes
+Dir.glob("views/pages/*.erb").each do |file|
+  page = File.basename(file, ".erb")
+  next if page == "layout" # Exclude the layout from being treated as a route
+  get "/#{page}" do
+    erb :"pages/layout", layout: :layout do
+      erb :"pages/#{page}"
+    end
+  end
+end
+
 not_found do
   status 404
   erb :"errors/404"
