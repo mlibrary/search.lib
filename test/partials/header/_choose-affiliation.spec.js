4ec20b499b8c1831e82f4f1d7992b3fb92a498f3
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-02-07 20:42:12 +0000

    Defining querySelector functions to reduce repetition.

--- test/partials/header/_choose-affiliation.spec.js
+++ test/partials/header/_choose-affiliation.spec.js
@@ -2,6 +2,11 @@ import chooseAffiliation from '../../../assets/scripts/partials/header/_choose-a
 import { expect } from 'chai';
 
 describe('chooseAffiliation', function () {
+  let getWebsiteHeader = null;
+  let getChangeAffiliation = null;
+  let getDialog = null;
+  let getDismissButtons = null;
+
   beforeEach(function () {
     // Apply HTML to the body
     document.body.innerHTML = `
@@ -22,44 +27,34 @@ describe('chooseAffiliation', function () {
       </m-website-header>
     `;
 
-    // Mock HTMLDialogElement as it does not exist in JSDOM
-    global.HTMLDialogElement = class {
-      constructor () {
-        this.open = false;
-      }
-
-      showModal () {
-        this.open = true;
-      }
-
-      close () {
-        this.open = false;
-      }
+    getWebsiteHeader = () => {
+      return document.querySelector('m-website-header > nav > ul');
     };
-
-    // Apply mock methods to the dialog element in the DOM
-    document.querySelectorAll('dialog').forEach((dialog) => {
-      dialog.showModal = function () {
-        return dialog.setAttribute('open', 'true');
-      };
-      dialog.close = function () {
-        return dialog.removeAttribute('open');
-      };
-    });
+    getChangeAffiliation = () => {
+      return document.querySelector('.affiliation__change');
+    };
+    getDialog = () => {
+      return document.querySelector('.affiliation__dialog');
+    };
+    getDismissButtons = () => {
+      return document.querySelectorAll('.affiliation__dialog--dismiss');
+    };
   });
 
   afterEach(function () {
+    getWebsiteHeader = null;
+    getChangeAffiliation = null;
+    getDialog = null;
+    getDismissButtons = null;
+
     // Remove the HTML of the body
     document.body.innerHTML = '';
-    // Delete global variables
-    delete global.HTMLDialogElement;
   });
 
   describe('anchor', function () {
     it('should remain unchanged if `websiteHeader` is `null`', function () {
       // Get the unordered list element
-      const websiteHeader = 'm-website-header > nav > ul';
-      const unorderedList = document.querySelector(websiteHeader);
+      const unorderedList = getWebsiteHeader();
       const unorderedListHTML = unorderedList.innerHTML;
 
       // Change to an ordered list element
@@ -67,58 +62,40 @@ describe('chooseAffiliation', function () {
       const orderedList = document.querySelector('m-website-header > nav > ol');
       orderedList.innerHTML = unorderedListHTML;
 
-      // Retrieve anchor
-      const getAnchor = () => {
-        return document.querySelector('.affiliation__change');
-      };
-
       // Check if `.affiliation__change` is an anchor
-      const initialElement = getAnchor();
+      const initialElement = getChangeAffiliation();
       expect(initialElement, 'anchor element should exist before modification').to.not.be.null;
       expect(initialElement.tagName, 'initial element should be an anchor tag').to.equal('A');
 
       chooseAffiliation();
 
       // Check if `.affiliation__change` is still an anchor
-      const modifiedElement = getAnchor();
+      const modifiedElement = getChangeAffiliation();
       expect(modifiedElement, 'anchor element should still exist after modification').to.not.be.null;
       expect(modifiedElement.tagName, 'modified element should still be an anchor tag').to.equal('A');
     });
 
     it('should remain unchanged if `dialog` element does not exist', function () {
-      // Retrieve the dialog
-      const getDialog = () => {
-        return document.querySelector('.affiliation__dialog');
-      };
       expect(getDialog(), 'dialog should exist before modification').to.not.be.null;
 
       // Remove the dialog class, and retrieve again
       getDialog().classList.remove('affiliation__dialog');
       expect(getDialog(), 'dialog should no longer be found').to.be.null;
 
-      // Retrieve anchor
-      const getAnchor = () => {
-        return document.querySelector('.affiliation__change');
-      };
-
       // Check if `.affiliation__change` is an anchor
-      const initialElement = getAnchor();
+      const initialElement = getChangeAffiliation();
       expect(initialElement, 'anchor element should exist before modification').to.not.be.null;
       expect(initialElement.tagName, 'initial element should be an anchor tag').to.equal('A');
 
       chooseAffiliation();
 
       // Check if `.affiliation__change` is still an anchor
-      const modifiedElement = getAnchor();
+      const modifiedElement = getChangeAffiliation();
       expect(modifiedElement, 'anchor element should still exist after modification').to.not.be.null;
       expect(modifiedElement.tagName, 'modified element should still be an anchor tag').to.equal('A');
     });
 
     it('should remain unchanged if a dismiss button does not exist', function () {
-      // Retrieve the dismiss buttons
-      const getDismissButtons = () => {
-        return document.querySelectorAll('.affiliation__dialog--dismiss');
-      };
       expect(getDismissButtons(), 'at least one dismiss button should exist in order to close the dialog element').to.not.be.empty;
 
       // Remove the dismiss class, and retrieve again
@@ -127,39 +104,29 @@ describe('chooseAffiliation', function () {
       });
       expect(getDismissButtons(), 'no dismiss buttons should be found').to.be.empty;
 
-      // Retrieve anchor
-      const getAnchor = () => {
-        return document.querySelector('.affiliation__change');
-      };
-
       // Check if `.affiliation__change` is an anchor
-      const initialElement = getAnchor();
+      const initialElement = getChangeAffiliation();
       expect(initialElement, 'anchor element should exist before modification').to.not.be.null;
       expect(initialElement.tagName, 'initial element should be an anchor tag').to.equal('A');
 
       chooseAffiliation();
 
       // Check if `.affiliation__change` is still an anchor
-      const modifiedElement = getAnchor();
+      const modifiedElement = getChangeAffiliation();
       expect(modifiedElement, 'anchor element should still exist after modification').to.not.be.null;
       expect(modifiedElement.tagName, 'modified element should still be an anchor tag').to.equal('A');
     });
 
     it('should replace the anchor with a button', function () {
-      // Retrieve anchor
-      const getAnchor = () => {
-        return document.querySelector('.affiliation__change');
-      };
-
       // Check if the initial element is an anchor
-      const initialElement = getAnchor();
+      const initialElement = getChangeAffiliation();
       expect(initialElement, 'anchor element should exist before modification').to.not.be.null;
       expect(initialElement.tagName, 'initial element should be an anchor tag').to.equal('A');
 
       chooseAffiliation();
 
       // Check if the anchor has been replaced by a button
-      const modifiedElement = getAnchor();
+      const modifiedElement = getChangeAffiliation();
       expect(modifiedElement, 'button element should exist after modification').to.not.be.null;
       expect(modifiedElement.tagName, 'modified element should be a button tag').to.equal('BUTTON');
     });
@@ -183,36 +150,57 @@ describe('chooseAffiliation', function () {
   });
 
   describe('dialog', function () {
-    let buttonChangeAffiliation = null;
-    let dialog = null;
-
     beforeEach(function () {
+      // Mock HTMLDialogElement as it does not exist in JSDOM
+      global.HTMLDialogElement = class {
+        constructor () {
+          this.open = false;
+        }
+
+        showModal () {
+          this.open = true;
+        }
+
+        close () {
+          this.open = false;
+        }
+      };
+
+      // Apply mock methods to the dialog element in the DOM
+      document.querySelectorAll('dialog').forEach((dialogElement) => {
+        dialogElement.showModal = function () {
+          return dialogElement.setAttribute('open', 'true');
+        };
+        dialogElement.close = function () {
+          return dialogElement.removeAttribute('open');
+        };
+      });
+
       // Run the function before each test
       chooseAffiliation();
+    });
 
-      // Get the button and dialog elements
-      buttonChangeAffiliation = document.querySelector('button.affiliation__change');
-      dialog = document.querySelector('.affiliation__dialog');
+    afterEach(function () {
+      // Delete global variables
+      delete global.HTMLDialogElement;
     });
 
     it('should open the dialog when the new button is clicked', function () {
-      buttonChangeAffiliation.click();
-      expect(dialog.hasAttribute('open'), 'dialog element should have an `open` attribute').to.be.true;
+      getChangeAffiliation().click();
+      expect(getDialog().hasAttribute('open'), 'dialog element should have an `open` attribute').to.be.true;
     });
 
     it('should close the dialog when any dismiss button is clicked', function () {
-      // Get all dismiss buttons
-      const dismissButtons = document.querySelectorAll('.affiliation__dialog--dismiss');
-      expect(dismissButtons, 'at least one dismiss button should exist in order to close the dialog element').to.not.be.empty;
+      expect(getDismissButtons(), 'at least one dismiss button should exist in order to close the dialog element').to.not.be.empty;
 
-      dismissButtons.forEach((dismissButton) => {
+      getDismissButtons().forEach((dismissButton) => {
         // Show the dialog first
-        buttonChangeAffiliation.click();
-        expect(dialog.hasAttribute('open'), 'dialog element should have an `open` attribute').to.be.true;
+        getChangeAffiliation().click();
+        expect(getDialog().hasAttribute('open'), 'dialog element should have an `open` attribute').to.be.true;
 
         // Close the dialog
         dismissButton.click();
-        expect(dialog.hasAttribute('open'), 'dialog element should not have an `open` attribute').to.be.false;
+        expect(getDialog().hasAttribute('open'), 'dialog element should not have an `open` attribute').to.be.false;
       });
     });
   });
