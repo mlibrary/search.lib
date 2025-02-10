be764ae78ae71d3d3d4e99697a72fc9e895e3fa8
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-02-10 18:31:39 +0000

    Creating datastore landing pages, and adding content.

--- app.rb
+++ app.rb
@@ -4,35 +4,43 @@ require "ostruct"
 require_relative "lib/services"
 
 before do
-end
-
-helpers do
-  def add_to_icons(icon)
-    @icons << icon
-  end
+  @presenter = OpenStruct.new(title: "PLACEHOLDER_TITLE", icons: "dashboard,open_in_new,search")
 end
 
 get "/" do
   redirect to("/everything")
 end
 
-get "/everything" do
-  @presenter = OpenStruct.new(title: "PLACEHOLDER_TITLE", icons: "dashboard,open_in_new,search")
-  erb :"datastores/everything"
+[
+  {slug: "everything", title: "Everything"},
+  {slug: "catalog", title: "Catalog"},
+  {slug: "articles", title: "Articles"},
+  {slug: "databases", title: "Databases"},
+  {slug: "onlinejournals", title: "Online Journals"},
+  {slug: "guidesandmore", title: "Guides and More"}
+].each do |datastore|
+  get "/#{datastore[:slug]}" do
+    @presenter.title = datastore[:title]
+    erb :"datastores/layout", layout: :layout do
+      erb :"datastores/#{datastore[:slug]}"
+    end
+  end
 end
 
-# Read all files in views/pages to automatically create routes
-Dir.glob("views/pages/*.erb").each do |file|
-  page = File.basename(file, ".erb")
-  next if page == "layout" # Exclude the layout from being treated as a route
-  get "/#{page}" do
+[
+  {slug: "about-library-search", title: "About Library Search"},
+  {slug: "accessibility", title: "Accessibility"}
+].each do |page|
+  get "/#{page[:slug]}" do
+    @presenter.title = page[:title]
     erb :"pages/layout", layout: :layout do
-      erb :"pages/#{page}"
+      erb :"pages/#{page[:slug]}"
     end
   end
 end
 
 not_found do
+  @presenter.title = "404 - Page not found"
   status 404
   erb :"errors/404"
 end
