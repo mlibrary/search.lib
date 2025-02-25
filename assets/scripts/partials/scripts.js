803312dc91cd065132359ef9d9f24a44bb5d4ce3
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-02-25 14:35:24 -0500

    Removing `DOMContentLoaded` event listener.

--- assets/scripts/partials/scripts.js
+++ assets/scripts/partials/scripts.js
@@ -1,7 +1,5 @@
 import chooseAffiliation from './header/_choose-affiliation.js';
 import displaySearchTip from './header/_search.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  chooseAffiliation();
-  displaySearchTip();
-});
+chooseAffiliation();
+displaySearchTip();
