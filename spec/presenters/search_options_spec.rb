940b282a5ab4d65b021218d22fd1efe444697000
Monique Rio <mrio@umich.edu>
2025-02-25 21:37:52 +0000

    remove commented out text

--- spec/presenters/search_options_spec.rb
+++ spec/presenters/search_options_spec.rb
@@ -112,45 +112,6 @@ RSpec.describe Search::Presenters::BaseSearchOptions do
       expect(options[1].options.first.text).to include("Browse")
     end
   end
-
-  #   context "#datastore_search_options" do
-  #     it "lists search options specific to the datastore" do
-  #       expect(described_class.new.datastore_search_options).to include("...")
-  #     end
-  #     it "defaults to the first datastore if datastore is nil" do
-  #       expect(described_class.new.datastore_search_options).to include("...")
-  #     end
-  #   end
-
-  #   context "#datastore_search_tips" do
-  #     it "lists search tips specific to the datastore search options" do
-  #       expect(described_class.new.datastore_search_options).to include("...")
-  #     end
-  #   end
-
-  #   context "#show_optgroups?" do
-  #     it "checks if there is more than one group of search options" do
-  #       expect(described_class.new.datastore_search_options).to include("...")
-  #     end
-  #   end
-
-  #   context "#default_option" do
-  #     it "gives the first search option specific to the datastore" do
-  #       expect(described_class.new.datastore_search_options).to include("...")
-  #     end
-  #   end
-
-  #   context "#queried_option" do
-  #     it "lists search options specific to the datastore" do
-  #       expect(described_class.new.datastore_search_options).to include("...")
-  #     end
-  #     it "defaults to the first option if queried option does not exist" do
-  #       expect(described_class.new.datastore_search_options).to include("...")
-  #     end
-  #     it "defaults to the first option if the query contains operators" do
-  #       expect(described_class.new.datastore_search_options).to include("...")
-  #     end
-  #   end
 end
 
 RSpec.describe Search::Presenters::SearchOption do
