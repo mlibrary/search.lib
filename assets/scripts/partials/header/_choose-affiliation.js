fc4ad1eaf8d797c56cd4454aac3399cb53a9953e
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-03-05 12:34:59 -0500

    Removing early returns for form, submit button, and dialog checks as the script would error out anyway.

--- assets/scripts/partials/header/_choose-affiliation.js
+++ assets/scripts/partials/header/_choose-affiliation.js
@@ -1,26 +1,17 @@
 const chooseAffiliation = () => {
-  // Check if the change affiliation form exists
   const chooseAffiliationForm = document.getElementById('affiliation__form');
-  if (!chooseAffiliationForm) {
-    return;
-  }
-
-  // Check if the choose affiliation button and dialog exist, or else the button cannot open the dialog
   const buttonChangeAffiliation = chooseAffiliationForm.querySelector('.affiliation__change');
   const dialog = chooseAffiliationForm.querySelector('.affiliation__dialog');
-  if (!buttonChangeAffiliation || !dialog) {
-    return;
-  }
-
-  // Check if the dialog has dismiss buttons, or else the dialog cannot be closed
-  const buttonDismiss = dialog.querySelectorAll('.affiliation__dialog--dismiss');
-  if (!buttonDismiss.length) {
-    return;
-  }
 
   // Check if the dialog has a submit button, or else the action cannot be posted
   const buttonSubmit = dialog.querySelector('button[type="submit"]');
   if (!buttonSubmit) {
+    return;
+  }
+
+  // Check if the dialog has dismiss buttons, or else the dialog cannot be closed
+  const dismissButtons = dialog.querySelectorAll('.affiliation__dialog--dismiss');
+  if (!dismissButtons.length) {
     return;
   }
 
@@ -31,7 +22,7 @@ const chooseAffiliation = () => {
   });
 
   // Event listeners to close dialog on each dismiss button click
-  buttonDismiss.forEach((dismissButton) => {
+  dismissButtons.forEach((dismissButton) => {
     dismissButton.addEventListener('click', (event) => {
       event.preventDefault();
       dialog.close();
